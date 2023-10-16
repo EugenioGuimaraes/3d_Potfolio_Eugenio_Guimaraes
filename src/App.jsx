65317dc5cc271b2d2615ec8dc3fd69c70ead71e4
Eugenio Lobo <eugenio.guimaraes1112@gmail.com>
2023-10-16 18:35:24 -0300

    Fix: node_modules & path

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,15 @@
 import { useState } from 'react'
-import './App.css'
 import { BrowserRouter } from 'react-router-dom';
 import {
-  About, Contact, Experience, FeedBacks,
-  Hero, Navbar, Tech, Works, StarsCanvas
+  Hero,
+  Navbar,
+  About,
+  Tech,
+  Experience,
+  Works,
+  Feedbacks,
+  Contact,
+  StarsCanvas
 } from './components';
 
 
@@ -11,23 +17,22 @@ const App = () => {
   return (
     <BrowserRouter>
       <div className='relative z-0 bg-primary'>
-        <div className='bg-hero-pattern bg-cover
-        bg-no-repeat bg-center'>
-          <Navbar/>
-          <Hero/>
+        <div className='bg-hero-pattern bg-cover bg-no-repeat bg-center'>
+          <Navbar />
+          <Hero />
         </div>
-        <About/>
-        <Experience/>
-        <Tech/>
-        <Works/>
-        <FeedBacks/>
+        <About />
+        <Experience />
+        <Tech />
+        <Works />
+        <Feedbacks />
         <div className='relative z-0'>
-          <Contact/>
-          <StarsCanvas/>
+          <Contact />
+          <StarsCanvas />
         </div>
       </div>
     </BrowserRouter>
-  )
+  );
 }
 
-export default App
+export default App;
